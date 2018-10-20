4c6a58b0f12e0d211684c65edb701c8822c7042c
Ezana Bahta <ezanab2@gmail.com>
2018-10-20 12:58:32 -0400

    created a login page

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import './Login.css'
 
 class Login extends Component {
     render() {
         return (
-            <div>
+            <div className="loginContainer">
                 <div className="loginBox">
                     <div>
-                        <label>Username:</label><input type="text" name="username"></input>
+                        <label>Username:</label><input type="text" name="username"></input> 
+                        <Link className="signupbutton" to="/signup"> Sign-up</Link>
                     </div>
                     <br />
                     <div>
                         <label>Password:</label><input type="password" name="password"></input> 
+                        <Link className="loginbutton" to="/questions">Log-In</Link>
                     </div>
                 </div>
             </div>
